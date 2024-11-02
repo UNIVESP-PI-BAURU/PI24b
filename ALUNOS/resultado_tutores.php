0c53a1c85010e599e0318250e43fe516565c0d58
GuiSbeghenUnivesp <guisbeghenunivesp@gmail.com>
2024-11-01 21:32:52 -0300

    testes114

--- ALUNOS/resultado_tutores.php
+++ ALUNOS/resultado_tutores.php
@@ -49,7 +49,7 @@ unset($_SESSION['tutores_resultados']);
     <?php endif; ?>
 
     <!-- Resultados da Pesquisa -->
-    <div class="main-content">
+    <div class="main-result">
         <div class="result" style="padding: 1rem; background-color: #f9f9f9; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
              width: 100%; max-width: 400px; box-sizing: border-box; margin-top: 2rem;">
             <h2 style="text-align: center; margin-bottom: 1rem;">Resultados da Pesquisa de Tutores</h2>
